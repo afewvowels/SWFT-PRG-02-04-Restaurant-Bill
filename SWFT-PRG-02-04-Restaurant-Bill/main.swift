d74d86d124f2654a1d843218dc7bf8261b1de6c6
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2017-10-10 18:20:18 -0400

    Initial Commit

---
+++ SWFT-PRG-02-04-Restaurant-Bill/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SWFT-PRG-02-04-Restaurant-Bill
+//
+//  Created by Keith Smith on 10/10/17.
+//  Copyright © 2017 Keith Smith. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
