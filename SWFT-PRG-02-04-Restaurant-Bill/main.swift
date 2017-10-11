98fc0977c7e6f5a5f23ed4bc3d407fa7e9a7ad05
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2017-10-10 21:17:50 -0400

    Initial version tested and working

--- SWFT-PRG-02-04-Restaurant-Bill/main.swift
+++ SWFT-PRG-02-04-Restaurant-Bill/main.swift
@@ -5,8 +5,18 @@
 //  Created by Keith Smith on 10/10/17.
 //  Copyright © 2017 Keith Smith. All rights reserved.
 //
+//  Write a program that computes the tax and tip on a restaurant bill for a patron
+//  with a $88.67 meal charge. The tax should be 6.75 percent of the meal cost.
+//  The tip should be 20 percent fo the total after adding the tax. Display the meal cost,
+//  tax amount, tip amount, and total bill on the screen.
 
 import Foundation
 
-print("Hello, World!")
+let bill = 88.67
+let tax = 0.0675
+let tip = 0.2
 
+var mealCost = bill + (bill * tax) + ((bill + (bill * tax)) * tip)
+var mealOutput = String(format: "Total cost: $%.2f", mealCost)
+
+print(mealOutput)
